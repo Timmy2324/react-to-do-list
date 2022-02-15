7645d92065fb70348c6767ffeb2bbbec5445e242
Timmy2324 <salikhov.t.s@yandex.ru>
2022-02-15 23:17:25 +0300

    Updates

---
+++ precache-manifest.1a3e90686e261aa594232957bebd5158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e0ae95307a656d423deecddf80556b",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "3415a5419b50286dfc02",
+    "url": "/react-to-do-list/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "f7836f2b80d8aa563c47",
+    "url": "/react-to-do-list/static/js/2.6b39099d.chunk.js"
+  },
+  {
+    "revision": "b2b323168b47d3ad2cbed83af467569f",
+    "url": "/react-to-do-list/static/js/2.6b39099d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3415a5419b50286dfc02",
+    "url": "/react-to-do-list/static/js/main.cca04fe2.chunk.js"
+  },
+  {
+    "revision": "03e6d098d0b9baea38e7",
+    "url": "/react-to-do-list/static/js/runtime-main.31ca867a.js"
+  }
+]);
